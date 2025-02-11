37c6454a99511b38c7ebef2ea409f7fd7b8d956d
Camila Pérez <camila.perezz.design@gmail.com>
2025-02-10 22:49:30 -0300

    fetch products item list container

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,25 @@
-export const ItemListContainer = ({text}) => {
+import { useState, useEffect } from "react"
+import { useParams } from "react-router"
+import ItemList from './ItemList'
+
+function ItemListContainer () {
+    const [items, setItems] = useState([])
+    const { nameCategory } = useParams()    
+   
+
+    useEffect(() => {
+        const allProducts = `https://dummyjson.com/products`
+        const byCategory = `https://dummyjson.com/products/category/${nameCategory}`
+
+        fetch(nameCategory ? byCategory : allProducts)
+            .then(res => res.json())
+            .then(res => setItems(res.products))
+            
+    }, [nameCategory])
 
     return (
-        <div className="container">
-            <h1>{text}</h1>
-        </div>
+        <ItemList items={items} />
     )
 }
+
+export default ItemListContainer
